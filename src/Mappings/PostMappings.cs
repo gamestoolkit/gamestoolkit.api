e769cedfba9f110568eaeecccbfd0776294db1a4
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-23 17:42:08 -0500

    Added Post mappings

---
+++ src/Mappings/PostMappings.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using gamestoolkit.api.Commands;
+using gamestoolkit.api.Models;
+using gamestoolkit.api.ViewModels;
+
+namespace gamestoolkit.api.Mappings
+{
+    public class PostMappings : Profile
+    {
+        public PostMappings() 
+        {
+            #region Queries
+            CreateMap<Post, PostWithoutContent>();
+            #endregion
+
+            #region Commands
+            CreateMap<CreatePostCommand, Post>();
+            #endregion
+        }
+    }
+}
