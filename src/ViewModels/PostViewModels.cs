4631c7b92fda2aa261b3006b8952d04cfcf8d664
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-23 17:41:25 -0500

    Added view models

---
+++ src/ViewModels/PostViewModels.cs
@@ -0,0 +1,11 @@
+﻿namespace gamestoolkit.api.ViewModels
+{
+    public class PostWithoutContent
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public string Author { get; set; } = null!;
+        public string? PostImage { get; set; }
+    }
+}
