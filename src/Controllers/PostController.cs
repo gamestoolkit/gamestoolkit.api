7e09caf6e0f7498473ea508d1a8ca24c53286e57
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-23 17:42:18 -0500

    WIP implementing Post controller

---
+++ src/Controllers/PostController.cs
@@ -0,0 +1,56 @@
+﻿using gamestoolkit.api.Commands;
+using gamestoolkit.api.Models;
+using gamestoolkit.api.Queries;
+using gamestoolkit.api.Services;
+using gamestoolkit.api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gamestoolkit.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController : Controller
+    {
+        private readonly IPostQueries _postQueries;
+        private readonly PostServices _postServices;
+
+        public PostController(IPostQueries postQueries, PostServices postServices)
+        {
+            _postQueries = postQueries;
+            _postServices = postServices;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<List<PostWithoutContent>>> GetAllAsync()
+        {
+            return await _postQueries.GetAllPostsWithoutContentAsync();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        public async Task<CreationResponse> CreateAsync(CreatePostCommand createPostCommand)
+        {
+            var response = await _postServices.CreatePostAsync(createPostCommand);
+            return CreatedAtAction(
+                nameof(GetAllAsync),
+                new { },
+                response
+            );
+        }
+
+        [HttpPut]
+        public IActionResult UpdateAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
