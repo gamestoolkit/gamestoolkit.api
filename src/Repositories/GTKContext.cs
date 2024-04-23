6268d917614cbe88ffa454b7cf65f434ab7de6c5
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-23 17:38:09 -0500

    Created DbContexts for both dapper and EF

---
+++ src/Repositories/GTKContext.cs
@@ -0,0 +1,34 @@
+﻿using gamestoolkit.api.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace gamestoolkit.api.Repositories
+{
+    public class GTKContext : DbContext
+    {        
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Collection> Collections { get; set; }
+        public DbSet<PostCollection> PostCollections { get; set; }
+
+        public GTKContext(DbContextOptions<GTKContext> options) : base(options)
+        {
+
+        }
+    }
+
+    public class GTKDapperContext
+    {
+        private readonly IConfiguration _configuration = null!;
+        private readonly string _connectionString = null!;
+
+        public GTKDapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SqlConnection")!;
+        }
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
