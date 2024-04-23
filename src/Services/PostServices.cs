bd5a02f0c55a2dc7112d1ac052b6527a7887bde0
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-23 17:42:29 -0500

    WIP implementing Post services

---
+++ src/Services/PostServices.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using gamestoolkit.api.Commands;
+using gamestoolkit.api.Models;
+using gamestoolkit.api.Repositories;
+using gamestoolkit.api.ViewModels;
+
+namespace gamestoolkit.api.Services
+{
+    public class PostServices
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IMapper _mapper;
+        public PostServices(IPostRepository postRepository, IMapper mapper)
+        {
+            _postRepository = postRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<int> CreatePostAsync(CreatePostCommand createPostCommand)
+        {
+            var post = _mapper.Map<Post>(createPostCommand);            
+            return await _postRepository.CreatePostAsync(post);
+        }
+    }
+}
