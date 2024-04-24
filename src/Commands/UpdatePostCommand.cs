2b6a95788ea627361306c3f9464401dfde0293f2
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-24 15:15:20 -0500

    Implemented delete and update post

---
+++ src/Commands/UpdatePostCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gamestoolkit.api.Commands
+{
+    public class UpdatePostCommand
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        [Required]
+        public string Author { get; set; } = null!;
+        [Required]
+        public string ContentHtml { get; set; } = null!;
+        public string? PostImage { get; set; }
+    }
+}
--- src/Services/PostServices.cs
+++ src/Services/PostServices.cs
@@ -22,5 +22,39 @@ namespace gamestoolkit.api.Services
             var id = await _postRepository.CreatePostAsync(post);
             return new CreateResponse { Id = id };
         }
+
+        public async Task<NoContentResponse> UpdatePostAsync(int id, UpdatePostCommand updatePostCommand)
+        {
+            var post = _mapper.Map<Post>(updatePostCommand);
+            var oldPost = await _postRepository.GetPostByIdAsync(id);
+            if (oldPost is null)
+            {
+                return new NoContentResponse { StatusCode = System.Net.HttpStatusCode.NotFound };
+            }
+
+            post.Id = oldPost.Id;
+            post.Author ??= oldPost.Author;
+            post.Description ??= oldPost.Description;
+            post.Title ??= oldPost.Title;
+            post.ContentHtml ??= oldPost.ContentHtml;
+            post.PostImage ??= oldPost.PostImage;
+
+            await _postRepository.UpdatePostAsync(post);
+
+            return new NoContentResponse { StatusCode = System.Net.HttpStatusCode.NoContent };
+        }
+
+        public async Task<NoContentResponse> DeletePostAsync(int id)
+        {            
+            var postToDelete = await _postRepository.GetPostByIdAsync(id);
+            if (postToDelete is null)
+            {
+                return new NoContentResponse { StatusCode = System.Net.HttpStatusCode.NotFound };
+            }
+
+            await _postRepository.DeletePostAsync(postToDelete);
+
+            return new NoContentResponse { StatusCode = System.Net.HttpStatusCode.NoContent };
+        }
     }
 }
