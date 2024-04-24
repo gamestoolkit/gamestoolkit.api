0ad3dc28fb69d842ac7945eea5438069e29f83af
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2024-04-24 15:14:57 -0500

    Added new view models

--- src/ViewModels/ServiceResponseViewModels.cs
+++ src/ViewModels/ServiceResponseViewModels.cs
@@ -1,8 +1,18 @@
-﻿namespace gamestoolkit.api.ViewModels
+﻿using System.Net;
+
+namespace gamestoolkit.api.ViewModels
 {
-  public class CreateResponse
-  {
-    public int Id { get; set; } = 0;
-    public string Message { get; set; } = string.Empty;
-  }
+    public class BaseResponse
+    {
+        public HttpStatusCode StatusCode { get; set; } = HttpStatusCode.OK;
+    }
+
+    public class CreateResponse : BaseResponse
+    {
+        public int Id { get; set; } = 0;
+        public string Message { get; set; } = string.Empty;
+    }
+    public class NoContentResponse : BaseResponse
+    {
+    }
 }
